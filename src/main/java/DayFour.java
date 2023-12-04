abe1de7aef3c225965c549fce7b2e1329536a8f5
kaimueller <me@kaimueller-code.com>
2023-12-04 13:49:15 +0100

    Day Four Part Two

--- src/main/java/DayFour.java
+++ src/main/java/DayFour.java
@@ -15,8 +15,16 @@ public class DayFour {
         Path path = new File("src/main/resources/day_four_input.txt").toPath();
         String content = Files.readString(path);
         int points = solver.solvePartOne(content);
-
         System.out.println("Day 3, Part 1 : " + points);
+
+        int numberOfScratchcards = solver.solvePartTwo(content);
+        System.out.println("Day 3, Part 2 : " + numberOfScratchcards);
+    }
+
+    private int solvePartTwo(String content) {
+        List<DayFour.Card> cards = parseCards(content);
+        int[] cardsWithCopies = createCopies(cards);
+        return Arrays.stream(cardsWithCopies).sum();
     }
 
     private int solvePartOne(String content) {
@@ -37,6 +45,26 @@ public class DayFour {
 
     public int getPointsCount(List<Card> cards) {
         return cards.stream().map(Card::getPoints).reduce(0, Integer::sum);
+    }
+
+    //solves the problem with dynamic programming
+    public int[] createCopies(List<Card> cards) {
+        int[] result = new int[cards.size()];
+        Arrays.fill(result, 1); //initialise with one copy of each card
+        result[0] = 1;
+        for(int i=0; i< cards.size(); i++){
+            Card c =  cards.get(i);
+            assert(i == c.id()-1);
+            int count = result[i];
+            long matches = c.getMatches();
+            if (matches > 0){
+                for (int j=1; j <= matches; j++){
+                    result[i+j] += count;
+                }
+            }
+
+        }
+        return result;
     }
 
     public record Card (int id, Set<Integer> winningNumbers, List<Integer> numbers){
@@ -64,13 +92,17 @@ public class DayFour {
             return new Card(id, winningNumbers, numbers);
         }
         public int getPoints(){
-            long matches = numbers.stream().filter(winningNumbers::contains).count();
+            long matches = getMatches();
             if (matches == 0) return 0;
             int result = 1;
             for (int i = 1; i < matches; i++ ){
                 result *= 2;
             }
             return result;
+        }
+
+        public long getMatches() {
+            return numbers.stream().filter(winningNumbers::contains).count();
         }
     }
 }
--- src/test/java/DayFourTest.java
+++ src/test/java/DayFourTest.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -21,6 +22,21 @@ public class DayFourTest {
         assertEquals(8, cards.getFirst().numbers().size());
         int points = day.getPointsCount(cards);
         assertEquals(13, points);
-
      }
+
+    @Test
+    public void testPartTwo() throws IOException {
+        DayFour day = new DayFour();
+        Path path = new File("src/test/resources/day_four_test_input.txt").toPath();
+        String content = Files.readString(path);
+        List<DayFour.Card> cards = day.parseCards(content);
+        int[] cardsWithCopies = day.createCopies(cards);
+        assertEquals(1, cardsWithCopies[0]);
+        assertEquals(2, cardsWithCopies[1]);
+        assertEquals(4, cardsWithCopies[2]);
+        assertEquals(1, cardsWithCopies[5]);
+
+        int result = Arrays.stream(cardsWithCopies).sum();
+        assertEquals(30, result);
+    }
 }
