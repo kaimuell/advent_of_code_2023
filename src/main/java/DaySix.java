9211f3299bf1de06ce3496be09700c8333015981
kaimuell <info@kaimueller-art.de>
2023-12-06 09:04:22 +0100

    Solves Day

---
+++ src/main/java/DaySix.java
@@ -0,0 +1,59 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+import static utils.ParsingUtils.tryToParseAllNumbers;
+
+
+/**
+ * Solves <a href="https://adventofcode.com/2023/day/6">Advent of Code Day 6</a>
+ */
+
+public class DaySix {
+    public List<Long> times;
+    public List<Long> distances;
+
+    public static void main(String[] args) throws IOException {
+        DaySix solver = new DaySix();
+        Path path = new File("src/main/resources/day_six_input.txt").toPath();
+        String content = Files.readString(path);
+        solver.parseInput(content);
+
+        long result1 = solver.computeAllWinPossibilities();
+        System.out.println("Day 6, Part 1 : " + result1);
+
+        long result2 = solver.computePossibleWins(62737565L, 644102312401023L);
+        System.out.println("Day 6, Part 2 : " + result2);
+    }
+
+    public void parseInput(String content) {
+        List<String> lines = content.lines().toList();
+        this.times = tryToParseAllNumbers(lines.get(0).split(" "));
+        this.distances = tryToParseAllNumbers(lines.get(1).split(" "));
+    }
+
+    public long computePossibleWins(Long time, Long distance) {
+        long wins = 0L;
+        boolean reachedFirst = false;
+        for (long i = 1; i < time; i++){
+            long distanceTraveled = (time-i)*i;
+            if (distanceTraveled > distance) {
+                wins ++;
+                reachedFirst = true;
+            } else if (reachedFirst) {
+                break;
+            }
+        }
+        return wins;
+    }
+
+    public long computeAllWinPossibilities() {
+        long possibilities = 1;
+        for (int i=0; i < times.size(); i++){
+            possibilities *= computePossibleWins(times.get(i), distances.get(i));
+        }
+        return possibilities;
+    }
+}
---
+++ src/test/java/DaySixTest.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class DaySixTest {
+
+    @Test
+    public void testPartOne() throws IOException {
+        DaySix day = new DaySix();
+        Path path = new File("src/test/resources/day_six_test_input.txt").toPath();
+        String content = Files.readString(path);
+        day.parseInput(content);
+        Assertions.assertEquals(3, day.distances.size());
+        Assertions.assertEquals(3, day.times.size());
+        long wins = day.computePossibleWins(day.times.get(0), day.distances.get(0));
+        Assertions.assertEquals(4L, wins);
+        long winPossibilities = day.computeAllWinPossibilities();
+        Assertions.assertEquals(288, winPossibilities);
+
+        long p2 = day.computePossibleWins(71530L, 940200L);
+        Assertions.assertEquals(71503L, p2);
+    }
+}
